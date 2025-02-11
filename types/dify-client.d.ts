ac06892ad2a0d9352deaafa779e19ef66fd8ba6e
bendusy <bendusty@live.com>
2025-02-11 17:40:17 +0800

    1
    1

--- types/dify-client.d.ts
+++ types/dify-client.d.ts
@@ -1,1 +1,13 @@
-// 删除整个文件 
+declare module 'dify-client' {
+    export interface DifyConfig {
+        apiKey: string;
+        baseUrl?: string;
+    }
+
+    export class DifyClient {
+        constructor(config: DifyConfig);
+        // 添加其他需要的方法声明
+    }
+
+    export default DifyClient;
+} 
